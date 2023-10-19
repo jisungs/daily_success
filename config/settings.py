a50a7d1100c8d024f032553fa950e7eb83e0293e
Jisung Smart <jisung.smart@gmail.com>
2023-10-19 13:52:55 +0900

    Add category app, models, admin

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'board',
+
+    'board', # user information and dashboard
+    'category',
 ]
 
 MIDDLEWARE = [

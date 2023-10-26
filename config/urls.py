0b580f12fb3c2cb3931644c24aeac6ceb874dc7d
Jisung Smart <jisung.smart@gmail.com>
2023-10-26 14:45:33 +0900

    hero 이미지 교체, 스토어 앱에서 상품 표시 되도록 수정, 필터 메뉴 작동하도록 동적 css selector를 활용하여 필터 기능 제작

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('store/', include('store.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
+from category.models import Category
 
 # Create your views here.
+
+def store(request):
+    products = Product.objects.all().filter(is_available=True)
+    categories = Category.objects.all()
+    
+    context = {
+        'products' : products,
+         'categories' : categories,
+    }
+        
+    return render(request, 'store/store.html', context)
